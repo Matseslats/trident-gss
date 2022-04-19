82561991f130f92dc538f58e395b1d947e317b28
Mats <49249061+Matseslats@users.noreply.github.com>
2022-04-19 21:26:47 +0200

    Blink test

--- python-edl/datalogger.py
+++ python-edl/datalogger.py
@@ -1,4 +1,5 @@
 # Temp: GPIO3
+# scp C:\Users\matsh\Documents\cansat\software\gss\trident-gss\python-edl\datalogger.py trident@10.0.0.15:/home/trident
 temppin = 3
 # Steg 1: Få temp data, manglende resistor mellom data og VCC?
 # Steg 2: Send data
@@ -11,12 +12,24 @@ temppin = 3
 
 
 import time
-from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
+# from w1thermsensor import W1ThermSensor
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.output(24,GPIO.HIGH)
+# sensor = W1ThermSensor()
+
 
 while True:
-    temperature = sensor.get_temperature()
-    print("The temperature is %s celsius" % temperature)
+    # temperature = sensor.get_temperature()
+    # print("The temperature is %s celsius" % temperature)
     time.sleep(1)
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(23,GPIO.LOW)
 
-    
+
---
+++ python-edl/startup.sh
@@ -0,0 +1,1 @@
+python datalogger.py
