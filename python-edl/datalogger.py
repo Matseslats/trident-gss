f297c62f1c10bc39606097aae3998440ff183327
Mats <49249061+Matseslats@users.noreply.github.com>
2022-04-19 13:10:10 +0200

    Update datalogger.py

--- python-edl/datalogger.py
+++ python-edl/datalogger.py
@@ -8,31 +8,8 @@ temppin = 3
 # Steg 6: PID kontroll
 
 # Få temp data
-import time
-import machine
-import onewire, ds18x20
-
-# the device is on GPIO3
-dat = machine.Pin(3)
-
-# create the onewire object
-ds = ds18x20.DS18X20(onewire.OneWire(dat))
-
-# scan for devices on the bus
-roms = ds.scan()
-print('found devices:', roms)
-
-# loop 10 times and print all temperatures
-for i in range(10):
-    print('temperatures:', end=' ')
-    ds.convert_temp()
-    time.sleep_ms(750)
-    for rom in roms:
-        print(ds.read_temp(rom), end=' ')
-    print()
 
 
-"""Eller:
 import time
 from w1thermsensor import W1ThermSensor
 sensor = W1ThermSensor()
@@ -41,4 +18,3 @@ while True:
     temperature = sensor.get_temperature()
     print("The temperature is %s celsius" % temperature)
     time.sleep(1)
-    """
